562cd30a025c6467fc8c938d934cab20b20b8cd6
Clifford Tran <clipqq>
2020-01-28 22:47:23 -0800

    basic functionality

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-// const searchUrl = ''
-// const apiKey = 'X1-ZWz1gmnxdi26tn_8tgid'
-// EXAMPLE ----- 
-// http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1gmnxdi26tn_8tgid&state=wa&city=seattle&childtype=neighborhood
-
-// https://api.jquery.com/jQuery.parseXML/
-
-//// School Digger API Info
-const apiKey = 'appID=5ae53d01&appKey=f3150e7d786a6d5de5cd3232ee0b4119'
-const searchUrl = `https://api.schooldigger.com/v1.2/schools?${apiKey}&sortBy=rank&`
-const schoolRankUrl = `${searchUrl}/v1.2/schools?`
-// https://api.schooldigger.com/v1.2/schools?st=ca&city=san%20francisco&appID=5ae53d01&appKey=f3150e7d786a6d5de5cd3232ee0b4119
-
+//// Weather API
+const apiKey = '&key=99646a4685984166bf6dae4cdc8c41c1'
+const baseWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=`
+// &city=Raleigh,NC
 
 //// Google Maps API Info
 const googleApiKey = 'AIzaSyDnrutrppg3LubVZPkD9uo0a1Jme-l0V6E'
+const googleMapEmbedQueryUrl = `https://www.google.com/maps/embed/v1/search?q=`
 
 
 //=====================================================
 
-function apiStandardRequest(state, city, school_name, level, min_rank, max_rank) {
-    const urlString = createStandardApiString(state, city, school_name, level, min_rank, max_rank)
+function apiStandardRequest(city, state) {
+    const urlString = createStandardApiString(city, state)
     makeFetchRequest(urlString)
 }
 
-function createStandardApiString(state, city, school_name, level, min_rank, max_rank) {
-    const rawInput = [state, city, school_name, level, min_rank, max_rank]
+function createStandardApiString(city, state) {
+    const rawInput = [city, state]
 
     const userInput = rawInput.filter(Boolean);
     console.log(userInput)
@@ -34,14 +26,12 @@ function createStandardApiString(state, city, school_name, level, min_rank, max_
     // switch cases for various user inputs
     switch (userInput.length) {
         case 1:
-            urlString = `${searchUrl}st=${userInput[0]}`
+            urlString = `${baseWeatherUrl}${userInput[0]}`
         case 2:
-            urlString = `${searchUrl}st=${userInput[0]}&city=${userInput[1]}`
-            // case 3:
-            //     urlString = `${searchUrl}st=${userInput[0]}&city=${userInput[1]}&q=${userInput[2]}&qSearchSchoolNameOnly=true`
-            // case 4:
-            //     urlString = `${searchUrl}st=${userInput[0]}&city=${userInput[1]}&q=${userInput[2]}`
+            urlString = `${baseWeatherUrl}${userInput[0]},${userInput[1]}`
     }
+
+    urlString = `${urlString}${apiKey}`
 
     // `${searchUrl}${userInput}${apiKey}`
     console.log(`making URL  ${urlString.trim()}`)
@@ -67,39 +57,27 @@ function makeFetchRequest(urlString) {
 
 function renderHTML(jsonResult) {
     console.log(jsonResult)
+    // const resultArray = JSON.parse(jsonResult)
+    // console.log(`PARSED ARRAY`, resultArray)
+    
     $('.results').empty()
-    for (let i = 0; i < jsonResult.schoolList.length; i++) {
-        const name = jsonResult.schoolList[i].schoolName
-        const url = jsonResult.schoolList[i].url
-        const schoolId = jsonResult.schoolList[i].schoolid
-        const address = jsonResult.schoolList[i].address.html
-        // const score = (jsonResult.schoolList[i].rankHistory[0].averageStandardScore).toFixed(0)
-        // <div class='name' id='${score}'>Score: ${score}</div>
-
-        const level = jsonResult.schoolList[i].schoolLevel
-        const streetAddress = jsonResult.schoolList[i].address.street
-        const zipCode = jsonResult.schoolList[i].address.zip
+    for (let i = 0; i < jsonResult.data.length; i++) {
+        const date = jsonResult.data[i].datetime
 
         $('.results').append(`
-        <div class='name' id='${name}'>${name}</div>
-        <div class='name' id='${level}'>${level}</div>
-        <div class='id' id='${schoolId}'>ID: ${schoolId}</div>
-        <div class='address' id='${address}'>${address}</div>
-        <div class='url' id='${url}'>${url}</div>
+        <div class='date' id='${date}'>${date}</div>
         <br>
     `)
-
-        
     }
 }
 
 function renderMap(address) {
     console.log(address)
     const cleanAddress = address.replace(/ /g, '+')
-    const searchItem = `https://www.google.com/maps/embed/v1/search?q=${cleanAddress}&key=${googleApiKey}`
-    console.log(searchItem)
+    const mapUrl = `${googleMapEmbedQueryUrl}${cleanAddress}&key=${googleApiKey}`
+    console.log(mapUrl)
 
-    $('.google-map').attr('src', searchItem)
+    $('.google-map').attr('src', mapUrl)
 
 
     // console.log('rendered map')
@@ -110,37 +88,34 @@ function renderMap(address) {
     //         },
     //         zoom: 8
     //     })
-    }
+}
+//// Google's code || sets map to user's location
 
-    function getSchoolDetails(schoolId) {
-        // apiRequest(schoolId)
-    }
+function listenSubmit() {
+    $('form').submit(function (e) {
+        e.preventDefault();
+        const state = $('#state').val()
+        const city = $('#city').val()
 
-    function listenSubmit() {
-        $('form').submit(function (e) {
-            e.preventDefault();
-            const state = $('#state').val()
-            const city = $('#city').val()
-            const schoolName = $('#school-name').val()
+        renderMap(`${city}+${state}`)
+        apiStandardRequest(city, state)
 
-            console.log(state)
-            apiStandardRequest(state, city, schoolName)
-        });
-    }
+    });
+}
 
-    function listenSchoolNameClick() {
-        $('.results').on('click', $('.name'), function (e) {
-            const detailClicked = e.target.id
-            console.log(detailClicked)
+// function listenSchoolNameClick() {
+//     $('.results').on('click', $('.name'), function (e) {
+//         const detailClicked = e.target.id
+//         console.log(detailClicked)
 
-            // update static map box
-            renderMap(detailClicked)
-        })
-    }
+//         // update static map box
+//         renderMap(detailClicked)
+//     })
+// }
 
-    function main() {
-        listenSubmit()
-        listenSchoolNameClick()
-    }
+function main() {
+    listenSubmit()
+    // listenSchoolNameClick()
+}
 
-    $(main)
+$(main)
