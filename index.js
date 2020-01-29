15606650ec59ac4f133789d8ad1b508b2703049b
Clifford Tran <clipqq>
2020-01-29 10:45:52 -0800

    city title added

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const baseWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=`
 const googleApiKey = 'AIzaSyDnrutrppg3LubVZPkD9uo0a1Jme-l0V6E'
 const googleMapEmbedQueryUrl = `https://www.google.com/maps/embed/v1/search?q=`
 
-
 //=====================================================
 
 function apiStandardRequest(city, state) {
@@ -18,17 +17,19 @@ function apiStandardRequest(city, state) {
 function createStandardApiString(city, state) {
     const rawInput = [city, state]
 
-    const userInput = rawInput.filter(Boolean);
-    console.log(userInput)
+    const userInput = rawInput.filter(Boolean)
 
     let urlString = ''
 
     // switch cases for various user inputs
     switch (userInput.length) {
-        case 1:
-            urlString = `${baseWeatherUrl}${userInput[0]}`
         case 2:
+            console.log('case 2 fired')
             urlString = `${baseWeatherUrl}${userInput[0]},${userInput[1]}`
+            break
+        case 1:
+            console.log('case 1 fired')
+            urlString = `${baseWeatherUrl}${userInput[0]}`
     }
 
     urlString = `${urlString}${apiKey}`
@@ -36,40 +37,51 @@ function createStandardApiString(city, state) {
     // `${searchUrl}${userInput}${apiKey}`
     console.log(`making URL  ${urlString.trim()}`)
     return urlString.trim()
-
 }
 
 function makeFetchRequest(urlString) {
     fetch(urlString)
-        .then(function (response) {
+        .then(function(response) {
             if (response.ok === true) {
                 return response.json()
             }
             throw new Error(response.statusText)
         })
-        .then(function (jsonResult) {
+        .then(function(jsonResult) {
             renderHTML(jsonResult)
         })
-        .catch(function (error) {
-            console.log("hey something broke", error)
+        .catch(function(error) {
+            console.log('hey something broke', error)
         })
 }
 
 function renderHTML(jsonResult) {
-    console.log(jsonResult)
+    console.log(`JSON res: ${jsonResult}`)
     // const resultArray = JSON.parse(jsonResult)
     // console.log(`PARSED ARRAY`, resultArray)
 
     $('.results').empty()
+    const cityName = jsonResult.city_name
+    const stateCode = jsonResult.state_code
+    const countryCode = jsonResult.country_code
+
+    console.log(cityName, stateCode, countryCode)
+
+    $('.results').append(`
+    <div class='targetGeo' id='targetGeo'>
+    <h2>${cityName}, ${stateCode}, ${countryCode}</h2>
+    </div>
+
+    `)
+
     for (let i = 0; i < jsonResult.data.length; i++) {
         const date = jsonResult.data[i].datetime
         const temp = jsonResult.data[i].temp
         const descrip = jsonResult.data[i].weather.description
 
-
         $('.results').append(`
         <div class='date' id='${date}'>${date}</div>
-        <div class='temp' id='${temp}'>${temp}</div>
+        <div class='temp' id='${temp}'>Temperature: ${temp} Celsius</div>
         <div class='descrip' id='${descrip}'>${descrip}</div>
 
         <br>
@@ -78,13 +90,11 @@ function renderHTML(jsonResult) {
 }
 
 function renderMap(address) {
-    console.log(address)
+    console.log(`sending to GoogleMaps: ${address}`)
     const cleanAddress = address.replace(/ /g, '+')
     const mapUrl = `${googleMapEmbedQueryUrl}${cleanAddress}&key=${googleApiKey}`
-    console.log(mapUrl)
 
     $('.google-map').attr('src', mapUrl)
-
 
     // console.log('rendered map')
     // const map = new google.maps.Map($('#map-box'), {
@@ -98,15 +108,14 @@ function renderMap(address) {
 //// Google's code || sets map to user's location
 
 function listenSubmit() {
-    $('form').submit(function (e) {
-        e.preventDefault();
+    $('form').submit(function(e) {
+        e.preventDefault()
         const state = $('#state').val()
         const city = $('#city').val()
 
         renderMap(`${city}+${state}`)
         apiStandardRequest(city, state)
-
-    });
+    })
 }
 
 // function listenSchoolNameClick() {
