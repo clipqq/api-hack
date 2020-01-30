c8011bdccfb68cfd1381abf114341e6a2e48352e
clipqq <clifford.l.tran@gmail.com>
2020-01-29 18:34:36 -0800

    cleaned up console logs

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 //// Weather API
 const apiKey = '&key=99646a4685984166bf6dae4cdc8c41c1'
 const baseWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=`
-// &city=Raleigh,NC
 
 //// Google Maps API Info
 const googleApiKey = 'AIzaSyDnrutrppg3LubVZPkD9uo0a1Jme-l0V6E'
 const googleMapEmbedQueryUrl = `https://www.google.com/maps/embed/v1/search?q=`
-
-//=====================================================
 
 function apiStandardRequest(city, state, country) {
     const urlString = createStandardApiString(city, state, country)
@@ -15,10 +12,6 @@ function apiStandardRequest(city, state, country) {
 }
 
 function createStandardApiString(city, state, country) {
-    const rawInput = [city, state, country]
-
-    const userInput = rawInput.filter(Boolean)
-
     let urlString = ''
     let stateString = ''
     if(state) {
@@ -26,10 +19,7 @@ function createStandardApiString(city, state, country) {
         stateString = `,${state}`
     }
     urlString = `${baseWeatherUrl}${city}${stateString},${country}`
-
     urlString = `${urlString}${apiKey}`
-
-    // `${searchUrl}${userInput}${apiKey}`
     return urlString.trim()
 }
 
@@ -46,29 +36,22 @@ function makeFetchRequest(urlString) {
         })
         .catch(function(error) {
             alert('City, State, or Country combination not found. Please revise your search.')
-            console.log('hey something broke', error)
+            console.log('City, State, or Country combination not found. Please revise your search.', error)
         })
 }
 
 function renderHTML(jsonResult) {
-    console.log(`JSON res: ${jsonResult}`)
-    // const resultArray = JSON.parse(jsonResult)
-    // console.log(`PARSED ARRAY`, resultArray)
-
     $('.results').empty()
     const cityName = jsonResult.city_name
     const stateCode = jsonResult.state_code
     const countryCode = jsonResult.country_code
 
-    console.log(cityName, stateCode, countryCode)
     renderMap(`${cityName}+${stateCode}+${countryCode}`)
-
 
     $('.results').append(`
     <div class='targetGeo' id='targetGeo'>
     <h2>${cityName}, ${stateCode}, ${countryCode}</h2>
     </div>
-
     `)
 
     for (let i = 0; i < jsonResult.data.length; i++) {
@@ -87,7 +70,6 @@ function renderHTML(jsonResult) {
         <div class='temp' id='${temp}'>Average Temperature: ${temp}°C</div>
         <div class='descrip' id='${descrip}'>${descrip}</div>
 
-        
         <div class='hidden-details' id='hidden-details'
 
         <div class='lowTemp' id='${lowTemp}'>Low: ${lowTemp}°C</div>
@@ -105,10 +87,8 @@ function renderHTML(jsonResult) {
 }
 
 function renderMap(address) {
-    console.log(`sending to GoogleMaps: ${address}`)
     const cleanAddress = address.replace(/ /g, '+')
     const mapUrl = `${googleMapEmbedQueryUrl}${cleanAddress}&key=${googleApiKey}`
-
     $('.google-map').attr('src', mapUrl)
 }
 
@@ -118,21 +98,12 @@ function listenSubmit() {
         const state = $('#state').val()
         const city = $('#city').val()
         const country = $('#country').val()
-
         apiStandardRequest(city, state, country)
     })
 }
 
-function listenSchoolNameClick() {
-    // $('.results').on('click', $('.name'), function (e) {
-    //     const targetContainer = e.target.id
-    //     console.log(targetContainer)
-    // })
-}
-
 function main() {
     listenSubmit()
-    listenTileClick()
 }
 
 $(main)
